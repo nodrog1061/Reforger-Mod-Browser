81359d82b109833be7a539d65f9ad8bcf03f6f9c
Lewis Taylor <Lewis101taylor@gmail.com>
2022-06-08 21:46:33 +0100

    WIP image gallery

--- lib/workshop_browser_web/controllers/page_controller.ex
+++ lib/workshop_browser_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule WorkshopBrowserWeb.PageController do
   use WorkshopBrowserWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # render(conn, "index.html")
+    redirect(conn, to: "/modlist")
   end
 end
