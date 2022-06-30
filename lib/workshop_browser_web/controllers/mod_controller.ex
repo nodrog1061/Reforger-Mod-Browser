2f344924cff0f8d23d649ebb538af5b22fc57dbe
Lewis Taylor <Lewis101taylor@gmail.com>
2022-06-30 21:43:13 +0100

    Pagination

--- lib/workshop_browser_web/controllers/mod_controller.ex
+++ lib/workshop_browser_web/controllers/mod_controller.ex
@@ -12,8 +12,28 @@ defmodule WorkshopBrowserWeb.ModController do
   end
 
   defp getData(conn) do
-    quantity = 50
-    %{data: GetWorkshopList.get_by_popularity(1, "desc", quantity)}
+    quantity =
+      if conn.query_params["count"] == nil do
+        20
+      else
+        case Integer.parse(conn.query_params["count"]) do
+          {x,_} when x > 50 -> 50
+          {x,_} -> x
+          _ -> 20
+        end
+      end
+
+    page =
+      if conn.query_params["page"] == nil do
+        1
+      else
+        case Integer.parse(conn.query_params["page"]) do
+          {x,_} -> x
+          _ -> 1
+        end
+      end
+
+    %{data: GetWorkshopList.get_by_popularity(page, "desc", quantity), page: page, quantity: quantity}
   end
 
 end
