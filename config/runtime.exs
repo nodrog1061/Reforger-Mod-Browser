f62ce591c8ac18ae9eb0e738ff211c964dd3bef7
Lewis Taylor <Lewis101taylor@gmail.com>
2022-07-01 21:40:54 +0100

    fly deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -36,15 +36,19 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
   config :workshop_browser, WorkshopBrowserWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: "#{app_name}.fly.dev", port: 80, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 
